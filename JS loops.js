ae04dd07c5b4db6383247cab9773e23e1f42f650
Redninja1 <james@redninja.net>
2016-10-10 03:13:16 +0100

    initial commit

---
+++ JS loops.js
@@ -0,0 +1,13 @@
+
+for(var i = 1; i < 5; i++){
+    console.log(i);
+}
+var i = 1;
+while( i < 5){
+    console.log(i);
+    i++;
+}
+var nope = false;
+do {
+    console.log("nailed it");
+}while(nope);
